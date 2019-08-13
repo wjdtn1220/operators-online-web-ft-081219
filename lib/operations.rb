84b83d6d1ab37a741b69ec40c7e36e4b06bd8be5
Jeong Park <wjdtn1220@gmail.com>
2019-08-13 03:41:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+  40 <= speed && speed <= 60 ? false : true
 end
 	
 
